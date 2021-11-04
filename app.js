d708cae563a832ce25d4ce8194be2470b482351c
Jonathan Velkeneers <jonathanvelkeneers@hotmail.com>
2021-11-03 22:37:43 -0500

    add addMods

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 let tetrisBlocksRouter = require('./routes/tetrisBlocks');
+let addModsRouter = require('./routes/addMods');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tetris_blocks', tetrisBlocksRouter);
+app.use('/add_mods', addModsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
